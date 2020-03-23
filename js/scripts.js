2cbaef8b932804420ae4cdf02429c4735780ee9f
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-03-23 13:49:51 -0400

    additions to addListItem, getAll, and loadList functions

--- js/scripts.js
+++ js/scripts.js
@@ -35,10 +35,39 @@ var characterRepository = (function() {
       }
     };
 
+    function addListItem(character) {
+      var $listItem = $('<li></li>');
+      var $button = $('<button>'+character.name+'</button>');
+      $('body').append($listItem);
+      $($listItem).append($button);
+      console.log(character);
+    };
+
+    function getAll() {
+      return repository;
+    }
+
 return {
-  loadList: loadList
+  loadList: loadList,
+  addListItem: addListItem,
+  getAll: getAll
 };
 
 }());
 
-characterRepository.loadList()
+characterRepository.loadList();
+console.log(characterRepository.getAll());
+
+// attempts at looping over repository array with addListItem - hasn't worked so far
+// unsure why - anything to do with the fact that the current loadList function isn't returning anything?
+// jquery - trying to iterate over getAll output
+$.each(characterRepository.getAll(), function(index, character) {
+  console.log(character);
+});
+
+// trying to iterate over getAll using foreach
+characterRepository.getAll().forEach(function(character) {
+  console.log(character);
+  characterRepository.addListItem(character);
+  console.log(character);
+});
